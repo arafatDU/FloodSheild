576318efb1b22c7ca54115a8c5fed0623936afa7
ARAFAT <108474267+arafatDU@users.noreply.github.com>
2024-10-05 08:28:01 +0600

    errror handled

--- src/app/components/prediction.js
+++ src/app/components/prediction.js
@@ -27,10 +27,10 @@ export function ChangeView({ coords }) {
 }
 
 const customIcon = new L.Icon({
-  iconUrl: "/prediction.png", // Ensure you have this icon in your public folder
-  iconSize: [25, 25], // Size of the marker icon
-  iconAnchor: [17, 55], // Anchor point for the marker
-  popupAnchor: [1, -45], // Popup position relative to the marker
+  iconUrl: "/prediction.png",
+  iconSize: [25, 25],
+  iconAnchor: [17, 55],
+  popupAnchor: [1, -45],
 });
 
 export default function MapCoordinates() {
@@ -41,51 +41,41 @@ export default function MapCoordinates() {
   });
   const center = [geoData.lat, geoData.lng];
 
-  // Fetch data from all three APIs and merge them based on the 'id'
+  const [isClient, setIsClient] = useState(false); // Track if the component is client-side
+
   useEffect(() => {
+    setIsClient(true); // Set client-side flag after mount
+
     const fetchData = async () => {
       try {
-        // Fetch stations data
-        const stationResponse = await fetch(
-          "https://ffwc-api.bdservers.site/data_load/stations/"
-        );
+        const stationResponse = await fetch("https://ffwc-api.bdservers.site/data_load/stations/");
         const stationData = await stationResponse.json();
 
-        // Fetch forecast data
-        const forecastResponse = await fetch(
-          "https://ffwc-api.bdservers.site/data_load/seven-days-forecast-waterlevel-24-hours/"
-        );
+        const forecastResponse = await fetch("https://ffwc-api.bdservers.site/data_load/seven-days-forecast-waterlevel-24-hours/");
         const forecastData = await forecastResponse.json();
 
-        // Fetch modified observed data (recent water levels)
-        const observedResponse = await fetch(
-          "https://ffwc-api.bdservers.site/data_load/modified-observed/"
-        );
+        const observedResponse = await fetch("https://ffwc-api.bdservers.site/data_load/modified-observed/");
         const observedData = await observedResponse.json();
 
-        // Merge station data with forecast data and observed data based on the 'id'
         const mergedData = stationData.map((station) => ({
           ...station,
-          forecast: forecastData[station.id] || null, // Attach forecast if exists, otherwise null
-          observed: observedData[station.id] || null, // Attach observed data if exists, otherwise null
+          forecast: forecastData[station.id] || null,
+          observed: observedData[station.id] || null,
         }));
 
-        setStations(mergedData); // Store merged data in state
+        setStations(mergedData);
       } catch (error) {
         console.error("Error fetching station, forecast, or observed data:", error);
       }
     };
     fetchData();
-  }, []); // Empty dependency array means this will run once when the component mounts
+  }, []);
 
-  // Prepare chart data for visualization in the popup
   const prepareChartData = (station) => {
     if (!station || !station.forecast) return null;
-
     const forecastDates = Object.keys(station.forecast);
     const forecastLevels = Object.values(station.forecast).map((level) => parseFloat(level));
 
-    // Prepare the data for the chart (include danger level, highest water level, and recent observed level for visualization)
     const data = {
       labels: forecastDates,
       datasets: [
@@ -98,115 +88,93 @@ export default function MapCoordinates() {
           tension: 0.1,
         },
         {
-            label: "Recent Water Level (m)",
-            data: station.observed
-              ? new Array(forecastDates.length).fill(parseFloat(Object.values(station.observed)[0]))
-              : [],
-            borderColor: "rgba(0, 255, 0, 0.8)", // Green for recent water level
-            fill: false,
-            tension: 0.1,
-          },
+          label: "Recent Water Level (m)",
+          data: station.observed ? new Array(forecastDates.length).fill(parseFloat(Object.values(station.observed)[0])) : [],
+          borderColor: "rgba(0, 255, 0, 0.8)",
+          fill: false,
+          tension: 0.1,
+        },
         {
           label: "Danger Level (m)",
           data: new Array(forecastDates.length).fill(parseFloat(station.dangerlevel)),
-          borderColor: "rgba(255, 0, 0, 0.8)", // Red color for danger level
-          borderDash: [5, 5], // Dashed line for better distinction
+          borderColor: "rgba(255, 0, 0, 0.8)",
+          borderDash: [5, 5],
           fill: false,
           tension: 0.1,
         },
         {
           label: "Highest Water Level (m)",
           data: new Array(forecastDates.length).fill(parseFloat(station.riverhighestwaterlevel)),
-          borderColor: "rgba(255, 165, 0, 0.8)", // Orange color for highest water level
-          borderDash: [5, 5], // Dashed line for better distinction
+          borderColor: "rgba(255, 165, 0, 0.8)",
+          borderDash: [5, 5],
           fill: false,
           tension: 0.1,
         },
-
       ],
     };
-
     return data;
   };
 
-  return (
-    <MapContainer
-      center={center}
-      zoom={7}
-      style={{ height: "100%", width: "100%" }}
-    >
-      <LayersControl position="topright">
+  if (!isClient) {
+    return null; // Render nothing on the server-side
+  }
 
+  return (
+    <MapContainer center={center} zoom={7} style={{ height: "100%", width: "100%" }}>
+      <LayersControl position="topright">
         <BaseLayer checked name="Satellite - Google">
-          <TileLayer
-            url="https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}"
-            subdomains={["mt0", "mt1", "mt2", "mt3"]}
-            attribution="&copy; Google"
-          />
+          <TileLayer url="https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}" subdomains={["mt0", "mt1", "mt2", "mt3"]} attribution="&copy; Google" />
         </BaseLayer>
         <BaseLayer name="OpenStreetMap">
-          <TileLayer
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
+          <TileLayer attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
         </BaseLayer>
       </LayersControl>
 
-      {/* Loop through the stations and create a Marker for each */}
-      {stations
-        .filter((station) => station.forecast !== null) // Only show stations with forecast data
-        .map((station) => (
-          <Marker
-            key={station.id}
-            position={[parseFloat(station.lat), parseFloat(station.long)]}
-            icon={customIcon}
-          >
-            <Popup maxWidth={650} minWidth={450} closeButton={false} className="wide-popup">
-              <div style={{ width: '100%', padding: '10px' }}>
-                <p><strong>Station Name:</strong> {station.name}</p>
-                <p><strong>River:</strong> {station.river}</p>
-                <p><strong>Danger Level:</strong> {station.dangerlevel}</p>
-                <p><strong>Highest Water Level:</strong> {station.riverhighestwaterlevel}</p>
-                <p><strong>Recent Water Level:</strong> {station.observed ? Object.values(station.observed)[0] : "N/A"}</p>
+      {stations.filter((station) => station.forecast !== null).map((station) => (
+        <Marker key={station.id} position={[parseFloat(station.lat), parseFloat(station.long)]} icon={customIcon}>
+          <Popup maxWidth={650} minWidth={450} closeButton={false} className="wide-popup">
+            <div style={{ width: "100%", padding: "10px" }}>
+              <p><strong>Station Name:</strong> {station.name}</p>
+              <p><strong>River:</strong> {station.river}</p>
+              <p><strong>Danger Level:</strong> {station.dangerlevel}</p>
+              <p><strong>Highest Water Level:</strong> {station.riverhighestwaterlevel}</p>
+              <p><strong>Recent Water Level:</strong> {station.observed ? Object.values(station.observed)[0] : "N/A"}</p>
 
-                {/* Show chart only if forecast data exists */}
-                {station.forecast && (
-                  <div style={{ width: '100%', height: '350px' }}>
-                    <Line
-                      data={prepareChartData(station)}
-                      options={{
-                        scales: {
-                          x: { title: { display: true, text: "Date" } },
-                          y: { 
-                            title: { display: true, text: "Water Level (m)" },
-                            ticks: {
-                              callback: function (value) {
-                                if (value === parseFloat(station.dangerlevel)) {
-                                  return 'Danger Level';
-                                } else if (value === parseFloat(station.riverhighestwaterlevel)) {
-                                  return 'Highest Water Level';
-                                } else if (station.observed && value === parseFloat(Object.values(station.observed)[0])) {
-                                  return 'Recent Water Level';
-                                }
-                                return value;
+              {station.forecast && (
+                <div style={{ width: "100%", height: "350px" }}>
+                  <Line
+                    data={prepareChartData(station)}
+                    options={{
+                      scales: {
+                        x: { title: { display: true, text: "Date" } },
+                        y: {
+                          title: { display: true, text: "Water Level (m)" },
+                          ticks: {
+                            callback: function (value) {
+                              if (value === parseFloat(station.dangerlevel)) {
+                                return "Danger Level";
+                              } else if (value === parseFloat(station.riverhighestwaterlevel)) {
+                                return "Highest Water Level";
+                              } else if (station.observed && value === parseFloat(Object.values(station.observed)[0])) {
+                                return "Recent Water Level";
                               }
-                            }
-                          }
+                              return value;
+                            },
+                          },
                         },
-                        plugins: {
-                          legend: { display: true, position: 'top' },
-                        },
-                        responsive: true,
-                        maintainAspectRatio: false,
-                      }}
-                      height={300}
-                    />
-                  </div>
-                )}
-              </div>
-            </Popup>
-          </Marker>
-        ))}
+                      },
+                      plugins: { legend: { display: true, position: "top" } },
+                      responsive: true,
+                      maintainAspectRatio: false,
+                    }}
+                    height={300}
+                  />
+                </div>
+              )}
+            </div>
+          </Popup>
+        </Marker>
+      ))}
 
       <ChangeView coords={center} />
     </MapContainer>
